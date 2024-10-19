49ac51abfc424a3bd825a51972534b2b471248cc
leo <jaykang1992@naver.com>
2024-10-20 00:42:31 +0900

    init

--- src/main/kotlin/com/example/hhplusweek3/domain/validator/CreatePaymentCommandValidator.kt
+++ src/main/kotlin/com/example/hhplusweek3/domain/validator/CreatePaymentCommandValidator.kt
@@ -4,25 +4,35 @@ import com.example.hhplusweek3.domain.command.CreatePaymentCommand
 import com.example.hhplusweek3.domain.model.QueueStatus
 import com.example.hhplusweek3.domain.port.QueueRepository
 import com.example.hhplusweek3.domain.port.ReservationRepository
+import com.example.hhplusweek3.domain.port.WalletRepository
 import org.springframework.stereotype.Component
 
 @Component
 class CreatePaymentCommandValidator(
     private val reservationRepository: ReservationRepository,
-    private val queueRepository: QueueRepository
+    private val queueRepository: QueueRepository,
+    private val walletRepository: WalletRepository,
 ) {
-
     fun validate(command: CreatePaymentCommand) {
         command.validate()
 
-        val queue = queueRepository.findByToken(command.queueToken)
-            ?: throw RuntimeException("queue token not found. ${command.queueToken}")
+        val queue =
+            queueRepository.findByToken(command.queueToken)
+                ?: throw RuntimeException("queue token not found. ${command.queueToken}")
 
         if (queue.status != QueueStatus.ACTIVE) {
             throw RuntimeException("queue is not active. ${queue.status}")
         }
 
-        reservationRepository.findByTokenAndReservationId(command.queueToken, command.reservationId)
-            ?: throw RuntimeException("reservation token not found by ${command.queueToken} and ${command.reservationId}")
+        val reservation =
+            reservationRepository.findByTokenAndReservationId(command.queueToken, command.reservationId)
+                ?: throw RuntimeException("reservation token not found by ${command.queueToken} and ${command.reservationId}")
+
+        val wallet =
+            walletRepository.findByQueueToken(command.queueToken)
+                ?: throw RuntimeException("wallet token not found by ${command.queueToken}")
+        if (wallet.balance < reservation.amount) {
+            throw RuntimeException("${wallet.balance} cannot be less than ${reservation.amount}")
+        }
     }
 }
