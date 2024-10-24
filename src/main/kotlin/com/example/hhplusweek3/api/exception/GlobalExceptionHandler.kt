f94fedb0860c701195d98fbaec74f2fa6344755f
leo <jaykang1992@naver.com>
2024-10-25 00:44:46 +0900

    프레임워크별 글로벌 에러 핸들러를 통해 예외 로깅 및 응답 처리 핸들러 구현한다
    변경사항
    - RestControllerAdvice를 만들어 서버에서 발생하는 모든 예외를 일관된 에러 메세지 응답 형태로 변환하였습니다.
    - 에러가 발생했을때 어떤 에러가 발생했는지 로깅을 추가했습니다.
    
    사유
    - 에러를 발생 시킬떄는 일관된 예외 모델을 내려줌으로써 클라이언트에서 에러르 핸들링하는것을 쉽게 하는게 좋다고 생각했습니다. 따라서, 에러 메시지, 경로, 상태 코드 등을 상세히 포함하여 어떤 사유 떄문에 에러가 발생했는지 디버깅하기 쉽도록 만들었습니다. 또한, 예상치 못한 에러도 똑같은 에러 모델로 변환하여 내려줘, 일관된 컨벤션을 유지하였습니다.
    - 해당 에러가 발생했을떄 서버에 남은 로그 등으로 파악하기 쉽도록 추가하였습니다. 경우에 따라 요청도 로그에 추가 할 수 있습니다.

---
+++ src/main/kotlin/com/example/hhplusweek3/api/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,74 @@
+package com.example.hhplusweek3.api.exception
+
+import com.example.hhplusweek3.api.response.ErrorResponse
+import com.example.hhplusweek3.domain.model.exception.ApplicationException
+import com.example.hhplusweek3.domain.model.exception.ErrorCode
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    @ExceptionHandler(ApplicationException::class)
+    fun handleApiException(
+        ex: ApplicationException,
+        request: WebRequest,
+    ): ResponseEntity<ErrorResponse> {
+        logger.warn("Handled ApiException: ${ex.errorCode.name} - ${ex.message}")
+
+        val status = mapErrorCodeToHttpStatus(ex.errorCode)
+
+        val errorResponse =
+            ErrorResponse(
+                status = status.value(),
+                error = status.reasonPhrase,
+                errorCode = ex.errorCode.name,
+                errorMessage = ex.message ?: "",
+                path = extractPath(request),
+            )
+        return ResponseEntity(errorResponse, status)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleAllUncaughtException(
+        ex: Exception,
+        request: WebRequest,
+    ): ResponseEntity<ErrorResponse> {
+        logger.error("Unhandled exception occurred: ${ex.message}", ex)
+        val errorCode = ErrorCode.INTERNAL_SERVER_ERROR
+
+        val errorResponse =
+            ErrorResponse(
+                status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                error = HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase,
+                errorCode = errorCode.name,
+                errorMessage = ex.message ?: "Unknown error",
+                path = extractPath(request),
+            )
+        return ResponseEntity(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    private fun mapErrorCodeToHttpStatus(errorCode: ErrorCode): HttpStatus =
+        when (errorCode) {
+            ErrorCode.QUEUE_NOT_FOUND,
+            ErrorCode.RESERVATION_NOT_FOUND,
+            ErrorCode.WALLET_NOT_FOUND,
+            ErrorCode.CONCERT_DATE_NOT_FOUND,
+            ErrorCode.CONCERT_SEAT_NOT_FOUND,
+            -> HttpStatus.NOT_FOUND
+            ErrorCode.INVALID_QUEUE_STATUS,
+            ErrorCode.QUEUE_LIMIT_EXCEEDED,
+            ErrorCode.INVALID_RESERVATION,
+            ErrorCode.ALREADY_PAID_RESERVATION,
+            -> HttpStatus.BAD_REQUEST
+            ErrorCode.INSUFFICIENT_BALANCE -> HttpStatus.FORBIDDEN
+            ErrorCode.INTERNAL_SERVER_ERROR -> HttpStatus.INTERNAL_SERVER_ERROR
+        }
+
+    private fun extractPath(request: WebRequest): String = request.getDescription(false).substringAfter("uri=")
+}
--- src/main/kotlin/com/example/hhplusweek3/api/response/ErrorResponse.kt
+++ src/main/kotlin/com/example/hhplusweek3/api/response/ErrorResponse.kt
@@ -1,18 +1,22 @@
 package com.example.hhplusweek3.api.response
 
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.Instant
 
 @Schema(description = "에러 응답 객체")
 data class ErrorResponse(
     @Schema(
         description = "에러 코드",
-        example = "INVALID_INPUT"
+        example = "INVALID_INPUT",
     )
     val errorCode: String,
-
     @Schema(
         description = "에러 메시지",
-        example = "입력 값이 유효하지 않습니다."
+        example = "입력 값이 유효하지 않습니다.",
     )
-    val errorMessage: String
+    val errorMessage: String,
+    val timestamp: Instant = Instant.now(),
+    val status: Int,
+    val error: String,
+    val path: String,
 )
