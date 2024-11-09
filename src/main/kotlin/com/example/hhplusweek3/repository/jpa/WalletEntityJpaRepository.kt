ed0d534fef6b450dd0a8cc54fd0ea138b005caca
leo <jaykang1992@naver.com>
2024-11-09 14:15:24 +0900

    erase wallet pessimistic lock

--- src/main/kotlin/com/example/hhplusweek3/repository/jpa/WalletEntityJpaRepository.kt
+++ src/main/kotlin/com/example/hhplusweek3/repository/jpa/WalletEntityJpaRepository.kt
@@ -1,24 +1,8 @@
 package com.example.hhplusweek3.repository.jpa
 
 import com.example.hhplusweek3.repository.model.WalletEntity
-import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
 interface WalletEntityJpaRepository : JpaRepository<WalletEntity, Long> {
     fun findByQueueToken(queueToken: String): WalletEntity?
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select w from WalletEntity w  where w.queueToken = :queueToken")
-    fun findByQueueTokenWithPessimisticLock(
-        @Param("queueToken") queueToken: String,
-    ): WalletEntity?
-
-    @Lock(LockModeType.OPTIMISTIC)
-    @Query("select w from WalletEntity w  where w.queueToken = :queueToken")
-    fun findByQueueTokenWithOptimisticLock(
-        @Param("queueToken") queueToken: String,
-    ): WalletEntity?
 }
