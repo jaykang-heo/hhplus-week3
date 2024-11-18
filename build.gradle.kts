2b35de90f47e2a136b0a996d237e3868676f8a59
leo <jaykang1992@naver.com>
2024-11-18 22:14:47 +0900

    Step 17에 대한 코드를 작성한다

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 }
 
 dependencies {
+    runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -30,13 +31,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("com.github.ben-manes.caffeine:caffeine")
-
     implementation("org.redisson:redisson-spring-boot-starter:3.27.1")
-
-    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.kafka:spring-kafka")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.mockito:mockito-core:5.5.0")
@@ -44,8 +42,9 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
-
-//    testImplementation("org.testcontainers:testcontainers:1.19.7")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.testcontainers:testcontainers:1.19.7")
+    testImplementation("org.testcontainers:kafka:1.19.7")
 }
 
 kotlin {
